ac07352fd59b16f4269add000855de2414028904
Acezus <32319352+Acezus@users.noreply.github.com>
2017-10-28 22:18:20 +1100

    Update group.js

--- group.js
+++ group.js
@@ -1,38 +1,26 @@
-	const Discord = module.require("discord.js");
 
-    module.exports.run = async (client, message, args) => {
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-        if(message.member.roles.some(r=>["Mentor", "Important to Canon"].includes(r.name)))
-    {{{
-        let embed = new Discord.RichEmbed()
-		.setThumbnail("https://t6.rbxcdn.com/ee20266dc962d52904f91684b4ffcfc1")
-		.setColor("#FFFFFF")
-		
-		.addField("Ranks of the Brotherhood", 
-		"**Low:** Novice; Servant; Assistant; Soldier; Disciple; Mercenary; 																																																								**Middle:** Warrior; Veteran; Master; 																																																														   **High:** Assassin; Elite Assassin; Grand Assassin; Elder Assassin; Master Assassin; Arch Assassin; Dignitary; Mentor;", true)
-		
-		.addField("Forts of The Revolutionary Assassins", "[Outpost Faroe](https://www.roblox.com/games/957586006/Outpost-Faroe) - Currently the only fort of the Assassins.**Disclamer:** *All assests belong to Levantine Assassins Brotherhood and their deveolpers including but not limited to Solarical, Sk3let0n, Nekzus, Donnybrook, Akiyba and Matkovic*", true)
-		
-		.addField("Training Ares of The Revolutionary Assassins", "[Animus Island](https://www.roblox.com/games/995890337/Animus-Island) - This is the primary training grounds and only for The Revolutionary Assassins. All events are to be hosted here. Credit once again, to Levantine Assassins Brotherhood and their deveolpers.")
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+client.on('message', message = > {
+ if(message.mentions.users.size === 0) {
+	return message.reply("You know, there is something called a 'mention', use it. Example: **@[USERNAME]**").catch(console.error);
+    let userinfo = message.mentions.members.first().username;
 
-		.addField("Division Restrictions", "Just a heads up. You can only be in **ONE** main division and **TWO** sides max. Division ceremonies may occassionaly be hosted or Tryouts.\n\n**If you are a EA+ (Elder Assassin and up), you have permisson to join both Main Divisions**")
+	
+ let embed = new Discord.RichEmbed()
+	 .setAuthor(message.mentions.users.first().username)
+	 .setThumbnail(message.mentions.users.first().avatarURL)
+	 .setColor('#'+(Math.random()*0xFFFFFF<<0).toString(16))
+	 .addField("Full Username", `${message.mentions.users.first().username}#${message.mentions.users.first().discriminator}`)
+	 .addField("ID", message.mentions.users.first().id, true)
+	 .addField("Created At", message.mentions.users.first().createdAt);
+	 
+ message.channel.send({embed})
+}
 
-		.addField("Offical Divisions", 
-		"**Main Divisions:** [Guards](https://www.roblox.com/Groups/group.aspx?gid=3388832); [Blades](https://www.roblox.com/My/Groups.aspx?gid=3388812) ;																																																																																																													**Side Divisions:** [Mentors Divisions](https://www.roblox.com/Groups/group.aspx?gid=3430246); [Scholars](https://www.roblox.com/Groups/group.aspx?gid=3439697); [Angels](https://www.roblox.com/My/Groups.aspx?gid=3499603)")
+});
 
-		.setFooter("Credits to Acezus#8392")
-		.setTimestamp(new Date());
-	message.channel.sendEmbed(embed);	
-    }}} else{
-        let fakeembed = new Discord.RichEmbed()
-    .setColor("#F50000")
-    .setDescription("Sorry you do not have the **_Mentor_** role!")
-    .setTimestamp(new Date());
-    message.channel.sendEmbed(fakeembed);
-
-    }}
-    
-
-    module.exports.help = {
-    name:"group"
-    }
