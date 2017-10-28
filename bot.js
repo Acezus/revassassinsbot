e498d6871db0d99699b44aa0a8ee1533925e920e
Acezus <32319352+Acezus@users.noreply.github.com>
2017-10-28 22:16:41 +1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,6 +10,24 @@ client.on('message', message => {
     if (message.content === 'ping') {
     	message.reply('pong');
   	}
+  
+client.on('message', message = > {
+ if(message.mentions.users.size === 0) {
+	return message.reply("You know, there is something called a 'mention', use it. Example: **@[USERNAME]**").catch(console.error);
+    let userinfo = message.mentions.members.first().username;
+
+	
+ let embed = new Discord.RichEmbed()
+	 .setAuthor(message.mentions.users.first().username)
+	 .setThumbnail(message.mentions.users.first().avatarURL)
+	 .setColor('#'+(Math.random()*0xFFFFFF<<0).toString(16))
+	 .addField("Full Username", `${message.mentions.users.first().username}#${message.mentions.users.first().discriminator}`)
+	 .addField("ID", message.mentions.users.first().id, true)
+	 .addField("Created At", message.mentions.users.first().createdAt);
+	 
+ message.channel.send({embed})
+}
+
 });
 
 // THIS  MUST  BE  THIS  WAY
